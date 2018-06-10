ae79e1e2228683a195b7007bcbe8b7d101ea5fae
Nick Webster <nick@nick.geek.nz>
2018-06-10 14:47:28 +1200

    auto-hide menubar

--- main.ts
+++ main.ts
@@ -15,6 +15,7 @@ function createWindow() {
 	window = new BrowserWindow({
 		width: 1000,
 		height: 800,
+		autoHideMenuBar: true,
 		webPreferences: {
 			nodeIntegration: false,
 			preload: path.join(__dirname, 'preload.js')
