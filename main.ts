8574d12ff10ea3d47d7c926a807fa47b2f3e9c72
Nick Webster <nick@nick.geek.nz>
2019-06-15 23:56:38 +1200

    Starting work on packaging with the --no-sandbox flag on linux

--- main.ts
+++ main.ts
@@ -4,7 +4,8 @@ import * as url from 'url';
 
 const IS_DEV = process.argv.slice(2).includes('--is-dev');
 
-// TODO: Get running in environments with kernel.unprivileged_userns_clone not set to true
+// TODO: Get running in environments with kernel.unprivileged_userns_clone not set to true (with packaging)
+app.commandLine.appendSwitch('--no-sandbox');
 
 let window: BrowserWindow;
 
